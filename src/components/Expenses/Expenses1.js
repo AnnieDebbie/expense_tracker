e9972f9346ff4a7601ed0e870def21a9ccf4aeda
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-10-05 21:41:08 +0100

    Render lists of expenses dynamically

--- src/components/Expenses/Expenses1.js
+++ src/components/Expenses/Expenses1.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import ExpenseItem1 from "./ExpenseItem1";
 import Card1 from "../UI/Card1";
@@ -8,36 +8,22 @@ import ExpensesFilter from "./ExpenseFilter";
 const Expenses1 = (props) => {
   const [year, setChosenYear] = useState("2022");
 
-  function yearChangeHandler(pickedYear){
-    setChosenYear(pickedYear)
-    console.log(pickedYear)
+  function yearChangeHandler(pickedYear) {
+    setChosenYear(pickedYear);
+    console.log(pickedYear);
   }
 
   return (
     <div>
-      <ExpensesFilter year={year} onChangeYear={yearChangeHandler}/>
-
       <Card1 className="expenses">
-        <ExpenseItem1
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem1
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem1
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem1
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        <ExpensesFilter year={year} onChangeYear={yearChangeHandler} />
+        {props.items.map((expense) => (
+          <ExpenseItem1
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card1>
     </div>
   );
