0750cb4e0627e1c7da81bf32e00a53ac42666ff6
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-10-06 19:50:06 +0100

    Filter displayed expenses by selected year Change year in expenses object to match that in select year drop down Add default year  option in dropdown

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -3,22 +3,20 @@ import React, { useState } from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-
   function selectHandler(e) {
     e.preventDefault();
     props.onChangeYear(e.target.value);
-
   }
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={selectHandler}>
+        <select  onChange={selectHandler}>
           <option value="2025">2025</option>
           <option value="2024">2024</option>
           <option value="2023">2023</option>
-          <option value="2022">2022</option>
+          <option value="2022" selected>2022</option>
         </select>
       </div>
     </div>
--- src/components/Expenses/Expenses1.js
+++ src/components/Expenses/Expenses1.js
@@ -17,13 +17,19 @@ const Expenses1 = (props) => {
     <div>
       <Card1 className="expenses">
         <ExpensesFilter year={year} onChangeYear={yearChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem1
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {props.items
+          .filter((expense) => {
+            let filteredYear = +year === new Date(expense.date).getFullYear();
+            return filteredYear;
+          })
+          .map((expense) => (
+            <ExpenseItem1
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
       </Card1>
     </div>
   );
